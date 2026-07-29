22e41d82cea64665e46138e846d3c182b980680d
Vu Anh <vuanh30062004@gmail.com>
2026-07-29 16:29:59 +0700

    fix deploy

--- backend/server.js
+++ backend/server.js
@@ -50,4 +50,4 @@ const PORT = process.env.PORT || 5000;
 // Khởi chạy các cron jobs tự động hóa (gửi mail, cập nhật trạng thái đơn)
 setupCronJobs();
 
-app.listen(PORT, "0.0.0.0", () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
